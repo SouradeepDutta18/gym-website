321d36b8f33299f201eeac7304b6f8835badf8c7
Souradeep Dutta <souradeepdutta2001@gmail.com>
2022-01-25 18:57:16 +0530

    fixed major bugs

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var menu = document.querySelector(".menu");
 var open = document.querySelector(".open");
 var close=document.querySelector(".close");
-var body = document.querySelector(".body");
+var body = document.querySelector("body");
+var menus=document.querySelectorAll(".menu li")
 open.addEventListener("click",()=>{
     console.log("open clicked");
     menu.classList.toggle("nav-active");
@@ -9,11 +10,18 @@ open.addEventListener("click",()=>{
     close.classList.toggle("close-active");
 
 })
-close.addEventListener("click",()=>{
-    menu.classList.toggle("nav-active");
-    body.classList.toggle("scroll-off");
-    close.classList.toggle("close-active");
+
+
+
+menus.forEach((node)=>{
+    node.addEventListener("click",()=>{
+        menu.classList.toggle("nav-active");
+        body.classList.toggle("scroll-off");
+        close.classList.toggle("close-active");
+    })
 })
+
+
 window.addEventListener("resize",(e)=>{
     if(screen.width>1024){
         if(menu.classList.indexOf("nav-active")!=-1)
